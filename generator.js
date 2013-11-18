bc0411784c0549858a34d9fe33f728cb3e9f1e79
Daniel Reiter <semibmx@gmail.com>
2013-11-17 23:31:33 -0600

    added to generateSwaps

--- generator.js
+++ generator.js
@@ -1,13 +1,24 @@
 var generator = {
-	
-	generateSwaps:function(numBalls, maxHeight, period) {
-		var maxSwap = 10^period;
-		for (var i = 0; i < ; i++) {
+	// var numBalls, maxHeight, period;
 
+	generateSwaps:function(numBalls, maxHeight, period) {
+		// this.numBalls = numBalls;
+		// this.maxHeight = maxHeight;
+		// this.period = period;
+
+		// NOTE: this only works for throws up to 9. Make more general...
+		var maxSwap = Math.pow(10, (period+1)) - 1;
+		var validSwaps = new Array();
+		for (var curSwap = 0; curSwap < maxSwap; curSwap++) {
+			if (isValidSwap(curSwap)) {
+				validSwaps.push(curSwap);
+			}
 		}
+
+		return validSwaps;
 	},
 
-	validateSwap:function(swap, maxHeight, period) {
+	isValidSwap:function(swap, maxHeight, period) {
 		var residues = {};
 		for (var i = 0; i < swap.length; i++) {
 			var curThrow = numberForThrow(swap[i]); // handle a-z
