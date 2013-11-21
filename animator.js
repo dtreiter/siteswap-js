7b1d103a40b4ca8019f0740701acb0bb09367274
Daniel Reiter <semibmx@gmail.com>
2013-11-21 01:47:40 -0600

    added animator skeleton file

---
+++ animator.js
@@ -0,0 +1,16 @@
+var animator = {
+	var L_HAND_X = 0;
+	var R_HAND_X = 0;
+
+	animateSwap:function(swap) {
+		var 
+	},
+
+	getThrowEndX:function(theThrow, throwIndex) {
+		var endX = theThrow % 2 == 0 ? SAME_HAND_X : OTHER_HAND_X;
+	},
+
+	getBallPosition:function(theThrow, tickFrame) {
+
+	}
+}
