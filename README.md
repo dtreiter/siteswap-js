32eaa783887bae0bceea02698b8d3b8f247508c7
Daniel Reiter <semibmx@gmail.com>
2014-01-02 01:53:51 -0600

    generator working for throw heights greater than 9

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ siteswap-js
 ===========
 
 Siteswap generator and animator in Javascript
+
+
+TODO
+[X] Make generator work for throws > 9
+
+[] Get balls drawn on screen
+[] Animate ball path
--- generator.js
+++ generator.js
@@ -1,36 +1,49 @@
 var generator = {
-	// var numBalls, maxHeight, period;
 
 	generateSwaps:function(numBalls, maxHeight, period) {
-		// this.numBalls = numBalls;
-		// this.maxHeight = maxHeight;
-		// this.period = period;
+		if (maxHeight > 35 || period < 1 || numBalls < 1) // invalid inputs
+			return null;
 
-		// NOTE: this only works for throws up to 9. Make more general...
-		var maxSwap = Math.pow(10, (period+1)) - 1;
-		var validSwaps = new Array();
-		for (var curSwap = 0; curSwap < maxSwap; curSwap++) {
-			if (this.isValidSwap(curSwap, numBalls, maxHeight, period)) {
-				validSwaps.push(curSwap);
+		var maxSwap = "";
+		var tosses = "0123456789abcdefghijklmnopqrstuvwxyz";
+		for (var i = 0; i < period; i++) {
+			maxSwap += tosses[maxHeight];
+		}
+
+		validSwaps = new Array();
+		for (var curSwap = 0; curSwap < parseInt(maxSwap, maxHeight+1); curSwap++) {
+			var testSwap;
+			if (maxHeight > 9) {
+				testSwap = curSwap.toString(maxHeight+1);
+			} else {
+				testSwap = curSwap.toString();
+			}
+
+			if (this.isValidSwap(testSwap, numBalls, maxHeight, period)) {
+				validSwaps.push(testSwap);
 			}
 		}
 
 		return validSwaps;
 	},
 
+
+	/*
+	* Input swap is a string
+	*/
 	isValidSwap:function(swap, numBalls, maxHeight, period) {
 		var residues = {};
-		swap = swap.toString();
 		var avg = 0;
+
 		for (var i = 0; i < swap.length; i++) {
-			var curThrow = this.numberForThrow(swap[i]); // handle a-z
-			var residue = (curThrow+i) % period;
-			if (residues[residue] || curThrow > maxHeight || swap.length != period) {
+			var curToss = this.numberForToss(swap[i], maxHeight); // handle a-z
+			var residue = (curToss+i) % period;
+			if (residues[residue] || curToss > maxHeight || swap.length != period) {
 				return false;
 			} else {
 				residues[residue] = true;
 			}
-			avg += parseInt(swap[i]);
+			avg += curToss;
 		}
 		avg /= swap.length;
 		if (avg != numBalls)
@@ -39,8 +52,10 @@ var generator = {
 		return true;
 	},
 
-	numberForThrow:function(curThrow) {
-		return curThrow;
-	}
+	numberForToss:function(curToss, maxHeight) {
+		if (maxHeight > 9)
+			return parseInt(curToss, maxHeight+1);
+		return parseInt(curToss);
+	} 
 
 }
