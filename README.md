25e793202a0344743eeacdd33ee61ac28e509752
Daniel Reiter <semibmx@gmail.com>
2014-01-02 04:43:39 -0600

    removed duplicate patters

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ Siteswap generator and animator in Javascript
 
 TODO
 [X] Make generator work for throws > 9
+[X] Remove duplicate patterns
+[ ] Rotate siteswap into standard orientation (ie. 345 -> 534)
+[ ] Support multiplexes
+[ ] Support async patterns
 
-[] Get balls drawn on screen
-[] Animate ball path
+[ ] Get balls drawn on screen
+[ ] Animate ball path
+
+[ ] Make Snazzy UI (AngularUI?)
--- generator.js
+++ generator.js
@@ -1,6 +1,6 @@
 var generator = {
 
-	generateSwaps:function(numBalls, maxHeight, period) {
+	generateSwaps:function(numBalls, maxHeight, period, limit) {
 		if (maxHeight > 35 || period < 1 || numBalls < 1) // invalid inputs
 			return null;
 
@@ -9,9 +9,12 @@ var generator = {
 		for (var i = 0; i < period; i++) {
 			maxSwap += tosses[maxHeight];
 		}
+		if (maxHeight > 9)
+			maxSwap = parseInt(maxSwap, maxHeight+1);
 
-		validSwaps = new Array();
-		for (var curSwap = 0; curSwap < parseInt(maxSwap, maxHeight+1); curSwap++) {
+		var validSwaps = new Array();
+		var duplicates = {};
+		for (var curSwap = 0; curSwap <= maxSwap; curSwap++) {
 			var testSwap;
 			if (maxHeight > 9) {
 				testSwap = curSwap.toString(maxHeight+1);
@@ -20,7 +23,19 @@ var generator = {
 			}
 
 			if (this.isValidSwap(testSwap, numBalls, maxHeight, period)) {
+				if (duplicates[testSwap])
+					break;
+
+				// add duplicate swaps to hash
+				for (var i = 0; i < testSwap.length; i++) {
+					var dupSwap = testSwap.substring(i, testSwap.length) + testSwap.substring(0, i);
+					console.log(dupSwap);
+					duplicates[dupSwap] = true;
+				}
+
 				validSwaps.push(testSwap);
+				if (validSwaps.length > limit)
+					break;
 			}
 		}
 
